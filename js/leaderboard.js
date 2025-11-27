ea49e6044866e986a6489fc49ee040cc743cd022
Fabian H. <hensslerfabian@gmail.com>
2025-11-27 22:10:43 +0100

    feat(leaderboard): add highlighting of own group in leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,5 +1,6 @@
 document.addEventListener("swac_components_complete", () => {
     setPlaceHolderImages("../content/logo_round.png");
+    highlightOwnGroup();
 })
 
 function setPlaceHolderImages(path) {
@@ -9,5 +10,34 @@ function setPlaceHolderImages(path) {
         if (!img.getAttribute("src")) {
             img.src = path;
         }
+    }
+}
+
+async function highlightOwnGroup() {
+    try {
+        const group_id = localStorage.getItem("user_group_id");
+        const response = await fetch(`${window.location.origin}/SmartDataAirquality/smartdata/records/groups/${group_id}?storage=gamification`);
+
+        if (!response.ok) {
+            throw new Error('Error setting group header: ' + response.status);
+        }
+
+        const data = await response.json();
+
+        const name = data.records[0].name;
+
+        const rows = document.querySelectorAll("tr");
+        for (const row of rows) {
+            const nameCell = row.querySelector("td:nth-child(2)");
+            if (!nameCell) continue;
+
+            if (nameCell.textContent.trim() === name) {
+                row.classList.add("highlight");
+                break;
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
     }
 }
