40c958d7cff36df7f2b98b192cf7f52b630463e9
Fabian H. <hensslerfabian@gmail.com>
2025-11-28 15:05:12 +0100

    feat(leaderboard): load correct group icons

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,27 +1,34 @@
 document.addEventListener("swac_components_complete", () => {
-    setPlaceHolderImages("../content/logo_round.png");
-    highlightOwnGroup();
+    loadGroups();
 })
 
-function setPlaceHolderImages(path) {
-    const images = document.querySelectorAll("td img");
-
-    for (let img of images) {
-        if (!img.getAttribute("src")) {
-            img.src = path;
-        }
-    }
-}
-
-async function highlightOwnGroup() {
+async function loadGroups() {
+    const group_info = await getGroupInfo();
     const group_id = localStorage.getItem("user_group_id")
-
     const rows = document.querySelectorAll("tr");
     for (const row of rows) {
+        const current_id = row.dataset.g_id;
+        const img = row.querySelector("img");
+        const record = group_info.find(r => r.group_id == current_id);
+        const path = record ? `../content/profile/${record.picture}` : ""
+        img.src = path
         if (row.dataset.g_id == group_id) {
             row.classList.add("highlight");
-            break;
         }
     }
 
+}
+async function getGroupInfo() {
+    try {
+        const response = await fetch(`${window.location.origin}/SmartDataAirquality/smartdata/records/view_groups?storage=gamification`);
+
+        if (!response.ok) {
+            throw new Error('Error getting group info: ' + response.status);
+        }
+
+        return await response.json().then(data => data.records);
+
+    } catch (error) {
+        console.error(error);
+    }
 }
