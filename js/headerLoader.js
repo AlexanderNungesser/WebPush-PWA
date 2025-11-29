ccb44ad728f6a6fad390f81975598c6e5977fc49
Fabian H. <hensslerfabian@gmail.com>
2025-11-29 23:35:59 +0100

    refactor: change header display

--- js/headerLoader.js
+++ js/headerLoader.js
@@ -3,8 +3,8 @@ window.addEventListener('group_reload', () => {
 });
 
 async function setGroupInfo() {
-    const groupDisplay = document.getElementById("group");
-    groupDisplay.textContent = groupData.group_name;
+    const groupDisplay = document.getElementById("header_logo");
+    groupDisplay.src = `../content/profile/${groupData.picture}`;
 
     const streakDisplay = document.getElementById("streak")
     streakDisplay.textContent = groupData.streak
